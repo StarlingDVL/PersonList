c7f712aad432eb5c622721d7759ed6d77d5ccf5f
ISkvor <starlingy608@gmail.com>
2022-02-01 18:56:44 +0300

    setup storyboard update DataManager

---
+++ PersonList/DataManagers/DataManager.swift
@@ -0,0 +1,60 @@
+//
+//  DataManager.swift
+//  PersonList
+//
+//  Created by Юрий Скворцов on 01.02.2022.
+//
+
+class DataManager {
+    let names = [
+        "Tim",
+        "Jason",
+        "Sarah",
+        "Peter",
+        "Molly",
+        "Henry",
+        "Chris",
+        "Ron",
+        "Ann",
+        "John"
+    ]
+    
+    let surnames = [
+        "Robinson",
+        "Hellstorm",
+        "Shultz",
+        "Van Greten",
+        "Sterling",
+        "Johnson",
+        "Pots",
+        "Gonzales",
+        "Cloud",
+        "Linder"
+    ]
+    
+    let eMaeils = [
+        "mail_11@yahoo.com",
+        "mail_22@yahoo.com",
+        "mail_33@yahoo.com",
+        "mail_44@yahoo.com",
+        "mail_55@yahoo.com",
+        "mail_66@yahoo.com",
+        "mail_77@yahoo.com",
+        "mail_88@yahoo.com",
+        "mail_99@yahoo.com",
+        "mail_00@yahoo.com"
+    ]
+    
+    let phoneNumbers = [
+        "9102211",
+        "9992431",
+        "92406712",
+        "9743569",
+        "7582365",
+        "5557384",
+        "1426390",
+        "6874444",
+        "9876543",
+        "3456272"
+    ]
+}
